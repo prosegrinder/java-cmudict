f9bdba7203cfb81137c3ab6dba6b13c15445fa9b
David L. Day <dday376@gmail.com>
2018-02-17 22:45:41 -0500

    Added CmuDictTest class. Broken.

---
+++ src/test/java/com/prosegrinder/cmudict/CmuDictTest.java
@@ -0,0 +1,22 @@
+package com.prosegrinder.cmudict;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CmuDictTest {
+
+  @Rule
+  public final ExpectedException thrown = ExpectedException.none();
+
+  
+}
