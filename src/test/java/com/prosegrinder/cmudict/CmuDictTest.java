85223b222efe1d65281b3420b19fbe8812d0a5a4
David L. Day <dday376@gmail.com>
2018-02-18 23:03:06 -0500

    Removed unused test code.

--- src/test/java/com/prosegrinder/cmudict/CmuDictTest.java
+++ src/test/java/com/prosegrinder/cmudict/CmuDictTest.java
@@ -15,17 +15,6 @@ public class CmuDictTest {
   @Rule
   public final ExpectedException thrown = ExpectedException.none();
 
-  // @Test
-  // public final void testGetPhonemes() {
-  //   CmuDict cmuDict = new CmuDict();
-  //   List<String> frowningPhonemes = Arrays.asList("F", "R", "AW1", "N", "IH0", "NG");
-  //   assertEquals("frowning:", frowningPhonemes, cmuDict.getPhonemes("frowning"));
-  //   List<String> zurkuhlenPhonemes = Arrays.asList("Z", "ER0", "K", "Y", "UW1", "L", "AH0", "N");
-  //   assertEquals("zurkuhlen:", zurkuhlenPhonemes, cmuDict.getPhonemes("zurkuhlen"));
-  //   List<String> cafePhonemes = Arrays.asList("K", "AH0", "F", "EY1");
-  //   assertEquals("cafe:", cafePhonemes, cmuDict.getPhonemes("cafe"));
-  // }
-
   @Test
   public final void testDict() {
     int EXPECTED_SIZE = 135086;
